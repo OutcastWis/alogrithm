de3f9e515437b84c6f464df62eb37ba827a0bb05
王泽锦 王 <wzj402@hotmail.com>
2025-11-12 17:27:51 +0800

    add test for splay

--- tree/test.cpp
+++ tree/test.cpp
@@ -1,4 +1,5 @@
 #include <tree/scapegoat_tree.h>
+#include <tree/splay.h>
 
 #include <iostream>
 #include <cassert>
@@ -67,8 +68,41 @@ void test_time_compare_with_std_map() {
     std::cout << "std::map Search Time: " << std::chrono::duration_cast<MS>(end - start).count() << " ms\n";
 }
 
+void test_splay() {
+    wzj::splay tree;
+
+    tree.insert("apple", 1);
+    tree.insert("banana", 2);
+    tree.insert("cherry", 3);
+    tree.insert("date", 4);
+    tree.insert("elderberry", 5);
+
+    assert(tree.has("apple") == true);
+    assert(tree.has("banana") == true);
+    assert(tree.has("cherry") == true);
+    assert(tree.has("date") == true);
+    assert(tree.has("elderberry") == true);
+    assert(tree.has("fig") == false);
+
+    assert(tree.kth(1).second == 1);
+    assert(tree.kth(2).second == 2);
+    assert(tree.kth(3).second == 3);
+    assert(tree.kth(4).second == 4);
+    assert(tree.kth(5).second == 5);
+
+    tree.erase("cherry");
+    assert(tree.has("cherry") == false);
+    assert(tree.kth(3).first == "date");
+
+    tree.erase("apple");
+    assert(tree.has("apple") == false);
+    assert(tree.kth(1).first == "banana");
+}
+
 int main() {
     test_scapegoat_tree();
     test_time_compare_with_std_map();
+
+    test_splay();
     return 0;
 }
